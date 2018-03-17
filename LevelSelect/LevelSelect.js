80c8fae6f766b7a9f55b1994c3d2c26ce5fea6b6
Juan Cirilo <juancirilo194@yahoo.com>
2018-03-17 19:39:21 +0000

    sd

--- LevelSelect/LevelSelect.js
+++ LevelSelect/LevelSelect.js
@@ -1,10 +1,12 @@
 /* global $*/
 
-
-
 var s = window.localStorage;
 var arr;
 var file = [];
+
+$(".planet1").click(function(){
+    s.setItem("Tracker", JSON.stringify(file));
+});
 
 $(".planet2").click(function(){
     get();
@@ -14,13 +16,16 @@ function get(){
     var a;
     
     arr = JSON.parse( s.getItem("Tracker") );
+    
     arr.forEach(function(it){
-        a = it.planet1;
-        console.log(it.planet1);
+        a = it.Planet1;
+        console.log(a);
     });
+    
     if(a == true){
         window.location.href='../Planet_2/Planet_2.html';
     } else {
         alert("Complete Planet 1 First.");
     }
 }
+
--- Planet_1/Planet_1.js
+++ Planet_1/Planet_1.js
@@ -21,20 +21,11 @@ $(document).ready(function() {
        setInterval(function () {
            if ( y <= 450) {
                console.log(y);
-               $("#astronaut_man").css("top", 500);
-            y = 500;
+               $("#astronaut_man").css("top", 495);
+            y = 4950;
            }
        }, 1000);
     } 
-    
-    function checkPosY(y) { 
-     if( y >= 490) { 
-         setInterval(1000);
-         y = y + 40;
-     }  
-        
-    } 
-    
       function moveDown() { 
       y = y + 20;
       $('#astronaut_man').css("top", y);
@@ -78,46 +69,4 @@ function set(){
     s.setItem("Tracker", JSON.stringify(file));
     window.location.href='../LevelSelect/LevelSelect.html';
 }
-
-//   function leftArrowPressed() {
-//             var element = document.getElementById("astronaut_man");
-//             element.style.left = parseInt(element.style.left) - 5 + 'px';
-//             }
-
-//             function rightArrowPressed() {
-//             var element = document.getElementById("astronaut_man");
-//             element.style.left = parseInt(element.style.left) + 5 + 'px';
-
-//             }
-
-//             function upArrowPressed() {
-//             var element = document.getElementById("astronaut_man");
-//             element.style.top = parseInt(element.style.top) - 5 + 'px';
-//             }
-
-//             function downArrowPressed() {
-//             var element = document.getElementById("astronaut_man");
-//             element.style.top = parseInt(element.style.top) + 5 + 'px';
-//             }
-
-//             function moveSelection(evt) {
-//                 switch (evt.keyCode) {
-//                     case 37:
-//                     leftArrowPressed();
-//                     break;
-//                     case 39:
-//                     rightArrowPressed();
-//                     break;
-//                     case 38:
-//                     upArrowPressed();
-//                     break;
-//                     case 40:
-//                     downArrowPressed();
-//                     break;
-//                     }
-//                 }
-                
-//         function docReady()  {
-//           window.addEventListener('keydown', moveSelection);
-//         }
 });  
